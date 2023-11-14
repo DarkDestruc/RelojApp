f0aeb33eafe35b6b2da8b4fdd8c0f210fbcabd48
Moises Chavez <gchavez19956@gmail.com>
2023-11-13 21:28:22 -0500

    Agregar archivos de proyecto.

---
+++ RelojApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace RelojApp
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
